8f206a011922ee3014ec1018ed641fed90aca5b9
Omar Vicente Villalobos Castro <44873840+ovillalobos@users.noreply.github.com>
2021-08-19 16:15:59 -0700

    SEO - Index
    Se realizan cambios en el titulo y se agrega una nueva imagen

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 
   $seo_data = array(
     'url' => "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",
-    'title' => $component['company_name'].' | Home',
-    'title_sm' => $component['company_name_sm'].' | Home',
+    'title' => $component['company_name'].' | Welcome',
+    'title_sm' => $component['company_name_sm'].' | Welcome',
     'image_1' => $component['company_url'].'/assets/img/social-media-share/welcome-precious-custom-cabinets.jpg',
     'image_2' => $component['company_url'].'/assets/img/social-media-share/welcome-precious-custom-cabinets.jpg',
     'image_3' => $component['company_url'].'/assets/img/social-media-share/welcome-precious-custom-cabinets.jpg',
