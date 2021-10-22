5b511233f442e0fb4a3172188c27ee8faa849084
Omar Vicente Villalobos Castro <44873840+ovillalobos@users.noreply.github.com>
2021-10-21 17:00:30 -0700

    Instagram
    Social Media

--- components/config/social-media.php
+++ components/config/social-media.php
@@ -2,7 +2,7 @@
 
 return array(
     'yelp'      =>  "https://www.yelp.com/biz/precious-custom-cabinets-south-gate",
-    'twitter'   =>  "https://twitter.com/pcustomcabinets",
+    'twitter'   =>  "",
     'facebook'  =>  "https://www.facebook.com/preciouscustomcabinets",
     'dribbble'  =>  "",
     'instagram' =>  "https://www.instagram.com/preciouscabinets/",
